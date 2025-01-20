9299239191b2c5044c069f704474f0bcdf67e4c9
Natalia Dudek <nananatalia.dudek@gmail.com>
2025-01-20 10:43:41 +0100

    update on sign-up

--- website/authentication.py
+++ website/authentication.py
@@ -1,21 +1,23 @@
 # storing roots for the website
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 
 # define that this file is a blueprint of application
 # setting up a blueprint
 authentication = Blueprint("authentication", __name__)
 
-@authentication.route("/login")
+@authentication.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    # data = request.form
+    # print(data) printuje w dict dane from (hasło, username)
+    return render_template("login.html", text="Testing...")
 
 @authentication.route("/logout")
 def logout():
     return "<p>Logout</p>"
 
-@authentication.route("/sign-up")
+@authentication.route("/sign-up", methods=["GET", "POST"])
 def sign_up():
     return render_template("sign_up.html")
 
